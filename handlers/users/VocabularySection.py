5f1ebc8db56eb5dd0ba4d26ade072e48ede73330
Khamidullo <mukhammadjonovxamidullo@gmail.com>
2022-10-06 14:56:19 +0200

    updated

--- handlers/users/VocabularySection.py
+++ handlers/users/VocabularySection.py
@@ -9,53 +9,51 @@ import pandas as pd
 from loader import dp
 
 
-@dp.message_handler(text="Vocabulary", state="*")
+@dp.message_handler(text="Vocabulary", state=VocabularyState.firstPage)
 async def send_categories(message: Message, state: FSMContext):
     await message.answer("Hi! here you can improve your vocabulary by exercising!\n"
                          "Choose one of the categories", reply_markup=VocabularySection)
     await VocabularyState.secondPage.set()
 
 
-@dp.message_handler(text="Guessing Word", state="*")
+@dp.message_handler(text="Guessing Word", state=VocabularyState.secondPage)
 async def start_the_game(message: Message, state: FSMContext):
     game_state = True
     while game_state:
         voc = pd.read_csv('voc.csv')
-        a = np.array(voc['word'])
-        b = np.array(voc['meaning'])
-        c = np.zeros(len(a))
+        words = np.array(voc['word'])
+        definitions = np.array(voc['meaning'])
+        learnt_words = np.zeros(len(words))
         f = 0
         game_engine = True
         while game_engine:
-            for i in a:
-                if np.sum(c) == (len(a) * 2):
-                    await message.answer("this is the end, you want to start all over again?", reply_markup=options)
-                    if message.text == "yes":
-                        await start_the_game()
-                    if message.text == "no":
-                        game_engine = False
-                    else:
-                        game_engine = False
-                        game_state = False
-                if c[f] == 0 or c[f] == 1:
-                    await message.answer("Score " + i + "\n Meaning: " + b[i])
-                    rate = int(await message.answer("rate: \n --i did not know-- 😐 \n --now i know, ask me "
-                                                    "again-- 😅 \n -- i knew it, dont ask me again -- 😃"))
-                    if rate == 0:
-                        c[f] = 0
-                    elif rate == 1:
-                        c[f] = 1
-                    elif rate == 2:
-                        c[f] = 2
-                    await message.answer(c)
+            for word in words:
+                if learnt_words[f] == 0 or learnt_words[f] == 1:
+                    await message.answer("Score \n Meaning: ", definitions[f])
+                    await message.answer("rate: \n --i did not know-- 😐 \n --now i know, ask me "
+                                         "again-- 😅 \n -- i knew it, dont ask me again -- 😃")
+                    answer = message.text
+                    if answer == "0":
+                        learnt_words[f] = 0
+                    elif answer == "1":
+                        learnt_words[f] = 1
+                    elif answer == "2":
+                        learnt_words[f] = 2
+                    await message.answer(learnt_words)
                     f += 1
                 else:
                     f += 1
                     continue
-            await message.answer("result", c)
+            await message.answer("result", learnt_words)
             f = 0
+            if np.sum(learnt_words) == (len(words) * 2):
+                await message.answer("this is the end, you want to start all over again?", reply_markup=options)
+                if message.text == "yes":
+                    await start_the_game()
+                if message.text == "no":
+                    game_engine = False
         else:
-            state = False
+            game_state = False
 
 
 @dp.message_handler(text="back", state="*")
