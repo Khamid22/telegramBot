b18f8a81cc07a6d64e89ecb170def471207d5462
Khamidullo <mukhammadjonovxamidullo@gmail.com>
2022-10-07 11:04:53 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	handlers/users/VocabularySection.py

--- handlers/users/VocabularySection.py
+++ handlers/users/VocabularySection.py
@@ -8,6 +8,11 @@ import numpy as np
 import pandas as pd
 from loader import dp
 
+voc = pd.read_csv('voc.csv')
+words = np.array(voc['word'])
+definitions = np.array(voc['meaning'])
+learnt_words = np.zeros(len(words))
+
 
 @dp.message_handler(text="Vocabulary", state=VocabularyState.firstPage)
 async def send_categories(message: Message, state: FSMContext):
@@ -19,40 +24,23 @@ async def send_categories(message: Message, state: FSMContext):
 @dp.message_handler(text="Guessing Word", state=VocabularyState.secondPage)
 async def start_the_game(message: Message, state: FSMContext):
     game_state = True
-    while game_state:
-        voc = pd.read_csv('voc.csv')
-        words = np.array(voc['word'])
-        definitions = np.array(voc['meaning'])
-        learnt_words = np.zeros(len(words))
-        f = 0
-        game_engine = True
-
-        while game_engine:
-            for word in words:
-                if np.sum(learnt_words) == (len(words) * 2):
-                    await message.answer("this is the end, you want to start all over again?", reply_markup=options)
-                    if message.text == "yes":
-                        await start_the_game()
-                    if message.text == "no":
-                        game_engine = False
-                if learnt_words[f] == 0 or learnt_words[f] == 1:
-                    await message.bot.send_message(message.from_user.id, f'word : {word}', reply_markup=reveal)
-                    async with state.proxy() as data:
-                        data['definitions[f]'] = definitions[f]
-                    await reveal_the_answer()
-                    answer = await receive_message()
-                    if answer == "0":
-                        learnt_words[f] = 0
-                    elif answer == "1":
-                        learnt_words[f] = 1
-                    elif answer == "2":
-                        learnt_words[f] = 2
-                    f += 1
-                else:
-                    f += 1
-                    continue
-                await message.bot.send_message(message.from_user.id, f'Result {learnt_words}')
-                f = 0
+    f = 0
+    game_engine = True
+    while game_engine:
+        for word in words:
+            if np.sum(learnt_words) == (len(words) * 2):
+                await message.answer("this is the end, you want to start all over again?", reply_markup=options)
+                if message.text == "yes":
+                    await start_the_game()
+                if message.text == "no":
+                    game_engine = False
+            if learnt_words[f] == 0 or learnt_words[f] == 1:
+                await message.bot.send_message(message.from_user.id, f'word : {word}', reply_markup=reveal)
+                async with state.proxy() as data:
+                    data['definitions[f]'] = definitions[f]
+                    data['f'] = f
+                await reveal_the_answer()
+                await choose_one_option()
 
 
 @dp.message_handler(text="back", state="*")
@@ -74,8 +62,17 @@ async def reveal_the_answer(call: CallbackQuery, state: FSMContext):
 @dp.callback_query_handler(state=VocabularyState.thirdPage)
 async def choose_one_option(call: CallbackQuery, state: FSMContext):
     await call.answer(cache_time=60)
-    await receive_message(call.data)
+    async with state.proxy() as data:
+        f = data['f']
+    answer = call.data
+    if answer == "0":
+        learnt_words[f] = 0
+    elif answer == "1":
+        learnt_words[f] = 1
+    elif answer == "2":
+        learnt_words[f] = 2
+        f += 1
+    else:
+        f += 1
+    await call.bot.send_message(call.from_user.id, f'Result {learnt_words}')
 
-
-async def receive_message(data):
-    return data
